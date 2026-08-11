65eeb6db47152f7434b66deee330c4c3a2b83149
b240584-droid <b240584@skit.ac.in>
2026-08-11 08:55:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import "dotenv/config"
+import connectDB from "./config/connectDB.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.route.js"
+import userRouter from "./routes/user.route.js"
+
+
+const app = express()
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+
+}))
+
+
+
+
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+
+const PORT = process.env.PORT || 6000
+app.listen(PORT , ()=>{
+   console.log(`Server is running on the Port ${PORT}`) 
+   connectDB()
+})
