f4ce41cb902fd0fd48ae7f3b429c6b1d5f01ed42
b241693-cyber <b241693@skit.ac.in>
2026-09-02 08:43:11 +0530

    Refactor Auth component to integrate Redux
    Updated Auth component to use Redux for user data management and added isModel prop for conditional rendering.

--- pages/Auth.jsx
+++ pages/Auth.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setUserData } from '../redux/userSlice.js'
 import { signInWithPopup } from "firebase/auth";
 import { GiArtificialIntelligence } from "react-icons/gi";
 import { IoSparkles } from "react-icons/io5";
@@ -8,7 +9,9 @@ import { auth, provider } from "../utils/firebase";
 import axios from "axios";
 import { ServerUrl } from '../App';
 
-function Auth() {
+
+function Auth({isModel =false}) {
+  const dispatch = useDispatch()
   const handleGoogleAuth = async ()=>{
     try {
       const response = await signInWithPopup(auth,provider)
@@ -16,15 +19,17 @@ function Auth() {
       let name = User.displayName
       let email = User.email
       const result = await axios.post(ServerUrl + "/api/auth/google" , {name ,email},{withCredentials:true})
-      console.log(result.data)
+      dispatch(setUserData(result.data)) 
+
     } catch (error) {
       console.log(error)
+      dispatch(setUserData(null))
     }
   }
   return (
-    <div className="auth w-full min-h-screen flex items-center justify-center px-6 py-20 bg-[#f3f3f3]">
+    <div className={`w-full ${isModel ? "py-4" : "min-h-screen flex items-center justify-center px-6 py-20 bg-[#f3f3f3]"}`}>
       <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1.25 }}
-      className="w-full max-w-md p-8 rounded-3xl bg-white shadow-2xl border border-gray-200">
+      className={`w-full ${isModel ? "max-w-md p-8 rounded-3xl" : "max-w-lg p-12 rounded-3xl bg-white shadow-2xl border border-gray-200"}`}>
         <div className=" flex  items-center justify-center mb-6 gap-3"> 
           <div className='bg-black text-white p-2 rounded-3xl'><GiArtificialIntelligence size={20} /></div>
           <h2 className="text-lg font-semibold ">InterViewX</h2>
